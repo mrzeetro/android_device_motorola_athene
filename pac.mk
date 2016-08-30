8a3305cdfce831d29951a15d6a030041be2f1e1d
Shreps <shr3ps@gmail.com>
2016-08-30 14:32:10 +0200

    athene: PAC MM Bringup

--- pac.mk
+++ pac.mk
@@ -20,10 +20,17 @@ $(call inherit-product, device/motorola/athene/full_athene.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+PAC_BOOTANIMATION_NAME := 1080
+
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := cm_athene
+PRODUCT_NAME := pac_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
